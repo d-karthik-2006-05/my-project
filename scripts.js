74367394a8b09c2e67a841f2056c858ba1747812
unknown <karthik>
2024-09-22 16:43:03 +0530

    server.js script.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const userName = "Rob-"+Math.floor(Math.random() * 100000)
+const userName = "video-"+Math.floor(Math.random() * 100000)
 const password = "x";
 document.querySelector('#user-name').innerHTML = userName;
 
